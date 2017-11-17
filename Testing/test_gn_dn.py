1192999579ff1f948c7a2a3ad388e502f9d00969
schrodog <sky_sun_tg@hotmail.com>
2017-11-17 11:51:05 +0800

    new testing

--- Testing/test_gn_dn.py
+++ Testing/test_gn_dn.py
@@ -295,6 +295,90 @@ def ab2lab(image):
     # return class8_313_rh
     return class8_ab
 
+def discriminative_net(input1, input2):
+    '''
+    VGG16 :
+    > conv1_1 > relu1_1 > conv1_2 > relu1_2 > pool1
+    ===
+    > conv2_1 > relu2_1 > conv2_2 > relu2_2 > pool2
+    ===
+    > conv3_1 > relu3_1 > conv3_2 > relu3_2 > conv3_3 > relu3_3 > pool3
+    ===
+    > conv4_1 > relu4_1 > conv4_2 > relu4_2 > conv4_3 > relu4_3 > pool4
+    ===
+    > conv5_1 > relu5_1 > conv5_2 > relu5_2 > conv5_3 > relu5_3 > pool5
+    ===
+    > FC-4096 > FC-4096 > FC-100 > soft-max
+    '''
+    def FC_layer(input,out_channels):
+        if len(input.get_shape)==4:
+            shape=int(np.prod(input.get_shape()[1:]))
+        else:
+            shape=int(np.prod(input.get_shape()[-1]))
+            weight=tf.Variable(tf.truncated_normal([shape,out_channels],dtype=tf.float32,stddev=1e-1))
+            bias=tf.Variable(tf.constant(1.0,shape=[out_channels],dtype=tf.float32))
+            flat=tf.reshape(input,[-1,shape])
+            fc=tf.nn.bias_add(tf.matmul(flat,weight),bias)
+            fc=tf.nn.relu(fc)
+            return fc
+
+    def kernel_init(in_channels, out_channels):
+        weights = tf.Variable( tf.truncated_normal([3,3,in_channels,out_channels], dtype=tf.float32, stddev=1e-1) )
+        biases = tf.Variable(tf.constant(0.0,shape=[out_channels],dtype=tf.float32))
+
+        return weights,biases
+
+    def conv(net,out_channels,relu=True):
+        in_channels=net.get_shape()[3]
+        weights,biases=kernel_init(in_channels,out_channels)
+        conv=tf.nn.conv2d(net,weights,[1,1,1,1], padding="SAME")
+        conv=tf.nn.bias_add(conv,biases)
+        if relu:
+            conv=tf.nn.relu(namedtuple)
+        return conv
+
+    def pool(net):
+        return tf.nn.max_pool(net,ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME")
+
+    # 2x [batch, height, width, in_channels] => [batch, height, width, in_channels * 2]
+    input=tf.concat([input1,input2],axis=3)
+
+    # block1 : [batch, 256, 256, 6] => [batch, 128, 128, 64]
+    conv1_1_relu=conv(input,64)
+    conv1_2_relu=conv(conv1_1_relu,64)
+    pool1=pool(conv1_2_relu)
+
+    # block2 : [batch, 128, 128, 64] => [batch, 64, 64, 128]
+    conv2_1_relu=conv(pool1,128)
+    conv2_2_relu=conv(conv2_1_relu,128)
+    pool2=pool(conv2_2_relu)
+
+    # block3 : [batch, 64, 64, 128] => [batch, 32, 32, 256]
+    conv3_1_relu=conv(pool2,256)
+    conv3_2_relu=conv(conv3_1_relu,256)
+    conv3_3_relu=conv(conv3_2_relu,256)
+    pool3=pool(conv3_3_relu)
+
+    # block4 : [batch, 32, 32, 256] => [batch, 16, 16, 512]
+    conv4_1_relu=conv(pool3,512)
+    conv4_2_relu=conv(conv4_1_relu,512)
+    conv4_3_relu=conv(conv4_2_relu,512)
+    pool4=pool(conv4_3_relu)
+
+    #block5 : [batch, 16, 16, 512] => [batch, 8, 8, 512]
+    conv5_1_relu=conv(pool4,512)
+    conv5_2_relu=conv(conv5_1_relu,512)
+    conv5_3_relu=conv(conv5_2_relu,512)
+    pool5=pool(conv5_3_relu)
+
+    #block6 :FC layers
+    fc1=FC_layer(pool5,4096)
+    fc2=FC_layer(fc1,4096)
+    fc3=FC_layer(fc2,1000)
+    result= tf.sigmoid(pool5)
+
+    return result
+
 def main():
     # CROP_SIZE = 256
     def convert(image, crop_size):
@@ -302,6 +386,7 @@ def main():
 
     with tf.Session() as sess:
         # list_images = glob.glob(os.path.join('./imgs', '*.JPEG'))
+        OUTPUT_SIZE=256
         img_file = '../imgs/ILSVRC2012_val_00046524.JPEG'
         image = tf.image.decode_jpeg(tf.read_file(img_file))
         height, width = cv2.imread(img_file).shape[:2]
@@ -329,23 +414,21 @@ def main():
 
         output2 = np.transpose( (sess.run(outputs))[0,:,:,:], (2,0,1))
         a_ch , b_ch, l_ch = [output2[0,:,:], output2[1,:,:], sess.run(L_ch)]
-        a_ch_res = np.clip(sni.zoom(a_ch, (1.*224/64, 1.*224/64)),-500,500) [:,:,np.newaxis]
-        b_ch_res = np.clip(sni.zoom(b_ch, (1.*224/64, 1.*224/64)),200,200) [:,:,np.newaxis]
-        l_ch_res = np.clip(sni.zoom(l_ch, (1.*224/height, 1.*224/width)),0,100) [:,:,np.newaxis]
-
-        # print(sess.run( tf.reduce_mean (l_ch_res)  ))
-        # print(sess.run( tf.reduce_max (l_ch_res)  ))
-        # print(sess.run( tf.reduce_min (l_ch_res)  ))
-        print(width, height)
+        a_ch_res = np.clip(sni.zoom(a_ch, (1.*OUTPUT_SIZE/64, 1.*OUTPUT_SIZE/64)),-500,500) [:,:,np.newaxis]
+        b_ch_res = np.clip(sni.zoom(b_ch, (1.*OUTPUT_SIZE/64, 1.*OUTPUT_SIZE/64)),200,200) [:,:,np.newaxis]
+        l_ch_res = np.clip(sni.zoom(l_ch, (1.*OUTPUT_SIZE/height, 1.*OUTPUT_SIZE/width)),0,100) [:,:,np.newaxis]
 
         # abl = np.concatenate((a_ch_res, b_ch_res, l_ch_res),axis=2)
         abl = tf.concat([a_ch_res, b_ch_res, l_ch_res],axis=2)
-        # print(a_ch_res)
-        # rgb_image = np.clip(color.lab2rgb(abl),0,255).astype('unit8')
-        rgb_image = sess.run(lab_to_rgb(abl))
+        rgb_image = lab_to_rgb(abl)
 
-    plt.imshow(rgb_image)
-    plt.axis('off')
-    plt.show()
+
+        # dis_out = discriminative_net(abl,targets)
+        print(dis_out)
+
+    #
+    # plt.imshow(rgb_image)
+    # plt.axis('off')
+    # plt.show()
 
 main()
